c3cce5cbca3cbac0e93ea7ac3505ca9f40c35a56
criolodoido <victor.santos.cd@gmail.com>
2018-03-18 23:38:28 -0300

    corrigindo

--- core/views.py
+++ core/views.py
@@ -8,8 +8,8 @@ from sandubas.models import Sandubas
 def index(request):
 	#no posts, posso fazer no models um campo para data limite de exibicao, desta forma crio aqui no views a condicao para a exibicao do mesmo.
 	posts = Promocao.objects.filter(encerramento__gte=timezone.now()).order_by('-encerramento')
-	acais = Acai.objects.filter(datapublicacao__gt=timezone.now().date() - timedelta(days=7)).order_by('-datapublicacao')[0:3]
-	sandubas = Sandubas.objects.filter(datapublicacao__gt=timezone.now().date() - timedelta(days=7)).order_by('-datapublicacao')[0:3]
+	acais = Acai.objects.filter(datapublicacao__gt=timezone.now().date() - timedelta(days=7)).order_by('datapublicacao')[0:3]
+	sandubas = Sandubas.objects.filter(datapublicacao__gt=timezone.now().date() - timedelta(days=7)).order_by('datapublicacao')[0:3]
 	return render(request, 'core/index.html', {'posts': posts, 'acais': acais, 'sandubas': sandubas})
 
 def vital(request):
